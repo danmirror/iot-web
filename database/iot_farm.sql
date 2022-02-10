c5ff87f6c42fb56d8a3524143d4288649f2d32a4
danmirror <danuandreanid@gmail.com>
2022-02-11 00:24:17 +0700

    database

--- database/iot_farm.sql
+++ database/iot_farm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 10, 2022 at 12:45 PM
+-- Generation Time: Feb 11, 2022 at 12:23 AM
 -- Server version: 5.7.35-0ubuntu0.18.04.2
 -- PHP Version: 7.2.34-24+ubuntu18.04.1+deb.sury.org+1
 
@@ -39,7 +39,7 @@ CREATE TABLE `respon` (
 --
 
 INSERT INTO `respon` (`id`, `value1`, `value2`, `value3`, `value4`) VALUES
-(1, '0', '1', '1', '0');
+(1, '1', '1', '1', '0');
 
 -- --------------------------------------------------------
 
@@ -56,18 +56,6 @@ CREATE TABLE `tb_sensor` (
   `gas` varchar(10) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_sensor`
---
-
-INSERT INTO `tb_sensor` (`id`, `suhu1`, `suhu2`, `kelembaban1`, `kelembaban2`, `gas`, `time`) VALUES
-(1, '0', '0', '8', '0', '0', '2022-02-10 01:37:06'),
-(2, '3', '8', '8', '0', '0', '2022-02-10 01:37:40'),
-(3, '3', '7', '8', '3', '5', '2022-02-10 02:00:39'),
-(4, '4', '7', '8', '3', '5', '2022-02-11 02:30:03'),
-(5, '4', '7', '8', '3', '5', '2022-02-10 04:46:42'),
-(6, '4', '7', '8', '3', '5', '2022-02-10 04:58:48');
 
 --
 -- Indexes for dumped tables
@@ -98,7 +86,7 @@ ALTER TABLE `respon`
 -- AUTO_INCREMENT for table `tb_sensor`
 --
 ALTER TABLE `tb_sensor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
