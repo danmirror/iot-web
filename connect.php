e8dbe33fb80d8cd11f197716bd08258ca8691b67
danmirror <danuandreanid@gmail.com>
2022-02-12 18:20:49 +0700

    fix minify

--- connect.php
+++ connect.php
@@ -1,32 +1,32 @@
 <?php
-   include 'function.php';
-   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $sensor1 =$_POST["sensor1"];
-      $sensor2 =$_POST["sensor2"];
-      $sensor3 =$_POST["sensor3"];
-      $sensor4 =$_POST["sensor4"];
-      $sensor5 =$_POST["sensor5"];
-      $save  = insert($sensor1, $sensor2, $sensor3, $sensor4, $sensor5);
-      if($save){
-         echo "post done";
-      }
-      else{
-         echo "post failed";
-      }
-   }
-   else{
-      $sensor1 =$_GET["sensor1"];
-      $sensor2 =$_GET["sensor2"];
-      $sensor3 =$_GET["sensor3"];
-      $sensor4 =$_GET["sensor4"];
-      $sensor5 =$_GET["sensor5"];
-      // var_dump($sensor5);
-      $save  = insert($sensor1, $sensor2, $sensor3, $sensor4, $sensor5);
-      if($save){
-         echo "get done";
-      }
-      else{
-         echo "get failed";
-      }
+	include 'function.php';
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$sensor1 =$_POST["sensor1"];
+		$sensor2 =$_POST["sensor2"];
+		$sensor3 =$_POST["sensor3"];
+		$sensor4 =$_POST["sensor4"];
+		$sensor5 =$_POST["sensor5"];
+		$save  = insert($sensor1, $sensor2, $sensor3, $sensor4, $sensor5);
+		if($save){
+			echo "post done";
+		}
+		else{
+			echo "post failed";
+		}
+	}
+	else{
+		$sensor1 =$_GET["sensor1"];
+		$sensor2 =$_GET["sensor2"];
+		$sensor3 =$_GET["sensor3"];
+		$sensor4 =$_GET["sensor4"];
+		$sensor5 =$_GET["sensor5"];
+		// var_dump($sensor5);
+		$save  = insert($sensor1, $sensor2, $sensor3, $sensor4, $sensor5);
+		if($save){
+			echo "get done";
+		}
+		else{
+			echo "get failed";
+		}
    }
 ?>
